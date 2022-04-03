9c44e01554aab8cdefdce8b8141d25307829cdfe
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-04-04 00:38:29 +0200

    week 1 contest C look carefully

---
+++ ASCIS/Weekly_Contest_1/C_Look_Carefully.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+    char c;
+    cin >> c;
+    if (c == 'C' || c == 'h' || c == 'i')cout << "Yes";
+    else cout << "No";
+    return 0;
+}
