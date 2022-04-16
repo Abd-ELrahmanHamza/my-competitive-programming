db22f9eef0673862e51acd3c2a68eae200d5374a
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-04-16 16:51:53 +0200

    D. Crash Bandicoot Adventures 2

---
+++ ASCIS/Back_on_track/D_Crash_Bandicoot_Adventures_2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+vll primeFactors(ll n) {
+    vll primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0)
+            primes.push_back(i), n /= i;
+    }
+    if (n > 1)
+        primes.push_back(n);
+    return primes;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vll v = primeFactors(n);
+    cout << v.size();
+    return 0;
+}
