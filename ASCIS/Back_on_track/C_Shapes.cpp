766dc5b694cb38ac20b5a702c6c0328af83cf9f4
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-04-16 16:35:43 +0200

    c. Shapes

---
+++ ASCIS/Back_on_track/C_Shapes.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define vector v;
+
+int main() {
+    double l;
+    cin >> l;
+    cout << (l / 2.0) * sqrt(2);
+    return 0;
+}
