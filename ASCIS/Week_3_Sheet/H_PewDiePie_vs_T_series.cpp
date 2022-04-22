b144b5d9b65acd3464b9b75db9380ed616a3024b
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-04-22 02:10:27 +0200

    H. PewDiePie vs T-series

---
+++ ASCIS/Week_3_Sheet/H_PewDiePie_vs_T_series.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+ull fact(ull n) {
+    ull result = 1;
+    for (int i = 1; i <= n; i++)
+        result *= i;
+    return result;
+}
+
+ull ncr(ull n, ull r) {
+    ull result = 1;
+    for (ull i = n; i >= n - r + 1; i--)
+        result *= i;
+    ull result2 = fact(r);
+    return result / result2;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    ll sum = 0, cnt = 0;
+    for (char c: s) {
+        if (c == 'T')cnt++;
+        else {
+            sum += (cnt - 2 > 0 ? cnt - 2 : 0);
+            cnt = 0;
+        }
+    }
+    sum += (cnt - 2 > 0 ? cnt - 2 : 0);
+
+    cout << sum;
+    return 0;
+}
