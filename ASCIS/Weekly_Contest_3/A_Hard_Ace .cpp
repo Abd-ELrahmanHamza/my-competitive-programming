3b53911c6a3c1627a7fea4c6a17bc31f22d18892
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-06 19:57:11 +0200

    A. Hard Ace

---
+++ ASCIS/Weekly_Contest_3/A_Hard_Ace .cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+
+int main() {
+    unordered_map<char, string> uMap = {
+            {'0', "Zero"},
+            {'1', "One"},
+            {'2', "Two"},
+            {'3', "Three"},
+            {'4', "Four"},
+            {'5', "Five"},
+            {'6', "Six"},
+            {'7', "Seven"},
+            {'8', "Eight"},
+            {'9', "Nine"}};
+    ll n;
+    cin >> n;
+    while (n--) {
+        char c;
+        cin >> c;
+        cout << uMap[c] << " ";
+    }
+    return 0;
+}
