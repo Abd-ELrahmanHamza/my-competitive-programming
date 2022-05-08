8f1be7acab2efca63ae05ee493a3ee4f88d1def8
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-09 01:12:54 +0200

    A. 007

---
+++ ASCIS/Support_Sheet_A/A_007.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+
+int main() {
+    int n;
+    cin >> n;
+    set<char> s;
+    while (n--) {
+        char c;
+        cin >> c;
+        s.insert(c);
+    }
+    if (s.size() % 2 == 0)
+        cout << "Continue";
+    else
+        cout << "Stop";
+    return 0;
+}
