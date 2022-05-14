ce64729912c70389eca497359f64ac9f6abbc1bc
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-15 01:41:36 +0200

    D. Road to One

---
+++ ASCIS/Filtration_Contest_1/D_Road_to_One.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+vll prime_factors(ll n) {
+    vll primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.push_back(n);
+    return primes;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vll v = prime_factors(n);
+//    for (auto x: v)cout << x << " ";
+//    cout << endl;
+    cout << v.size();
+    return 0;
+}
