861a6690266b954abe6810788226912246ffd102
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-18 01:46:35 +0200

    E - Almost Prime

---
+++ ASCIS/Week_4_Practice/E_Almost_Prime.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+vll prime_factors(ll n) {
+    vll primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.push_back(n);
+    return primes;
+}
+
+set<ll> prime_factors_set(ll n) {
+    set<ll> primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.insert(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.insert(n);
+    return primes;
+}
+
+ll binary_search(ll s, ll e, ll k) {
+    ll middle = (s + e) / 2;
+    if (k == middle)
+        return 1;
+    else if (k < middle)
+        return 1 + binary_search(s, middle - 1, k);
+    else
+        return 1 + binary_search(middle + 1, e, k);
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    ll result = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (prime_factors_set(i).size() == 2) {
+            result++;
+        }
+    }
+    cout << result;
+    return 0;
+}
