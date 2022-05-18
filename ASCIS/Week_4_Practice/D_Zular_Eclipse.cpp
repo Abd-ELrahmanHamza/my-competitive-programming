dfc8567e5c94d923118740081175827024751bb8
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-18 19:13:28 +0200

    D. Zular Eclipse

---
+++ ASCIS/Week_4_Practice/D_Zular_Eclipse.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+vll prime_factors(ll n) {
+    vll primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.push_back(n);
+    return primes;
+}
+
+set<ll> prime_factors_set(ll n) {
+    set<ll> primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.insert(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.insert(n);
+    return primes;
+}
+
+ll binary_search(ll s, ll e, ll k) {
+    ll middle = (s + e) / 2;
+    if (k == middle)
+        return 1;
+    else if (k < middle)
+        return 1 + binary_search(s, middle - 1, k);
+    else
+        return 1 + binary_search(middle + 1, e, k);
+}
+
+vll prefix_sum(vll v) {
+    v.insert(v.begin(), 0);
+    vll s(v.size());
+    for (int i = 1; i < v.size(); ++i) {
+        s[i] += v[i] + s[i - 1];
+    }
+    return s;
+}
+
+ll sum_range(int s, int e, vll &preSum) {
+    return preSum[e] - preSum[s - 1];
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vll v(n);
+    for (ll &x: v)cin >> x;
+    sort(v.begin(), v.end());
+    vll preSum = prefix_sum(v);
+    ll q;
+    cin >> q;
+
+    while (q--) {
+        ll x;
+        cin >> x;
+        auto it = upper_bound(v.begin(), v.end(), x);
+        if (it - v.begin() == 0)
+            cout << -1 << endl;
+        else
+            cout << (it - v.begin()) << " " << sum_range(1, it - v.begin(), preSum) << endl;
+    }
+    return 0;
+}
+
+
+
