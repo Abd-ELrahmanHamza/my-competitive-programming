9fdfcad5743991a54224b0115da75df1d5f9c601
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-19 06:01:00 +0200

    B. What's Next?

---
+++ ASCIS/Week_4_Sheet_A/B_Whats_Next.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+vll prime_factors(ll n) {
+    vll primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.push_back(n);
+    return primes;
+}
+
+set<ll> prime_factors_set(ll n) {
+    set<ll> primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.insert(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.insert(n);
+    return primes;
+}
+
+ll binary_search_n(ll s, ll e, ll k) {
+    ll middle = (s + e) / 2;
+    if (k == middle)
+        return 1;
+    else if (k < middle)
+        return 1 + binary_search_n(s, middle - 1, k);
+    else
+        return 1 + binary_search_n(middle + 1, e, k);
+}
+
+vll prefix_sum(vll v) {
+    v.insert(v.begin(), 0);
+    vll s(v.size());
+    for (int i = 1; i < v.size(); ++i) {
+        s[i] += v[i] + s[i - 1];
+    }
+    return s;
+}
+
+ll sum_range(int s, int e, vll &preSum) {
+    return preSum[e] - preSum[s - 1];
+}
+
+ll binary_search_increasing(ll s, ll e, ll k, const vll &v) {
+    if (e < s)
+        return -1;
+
+    ll middle = s + (e - s) / 2;
+    if (k == v[middle])
+        return middle;
+    else if (k < v[middle])
+        return binary_search_increasing(s, middle - 1, k, v);
+    else
+        return binary_search_increasing(middle + 1, e, k, v);
+}
+
+ll binary_search_decreasing(ll s, ll e, ll k, const vll &v) {
+    if (e < s)
+        return -1;
+
+    ll middle = s + (e - s) / 2;
+    if (k == v[middle])
+        return middle;
+    else if (k > v[middle])
+        return binary_search_decreasing(s, middle - 1, k, v);
+    else
+        return binary_search_decreasing(middle + 1, e, k, v);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + 2 * (b - a);
+}
