4375da91d41be47019d44c39d1b030ba884a2707
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-28 23:14:57 +0200

    D. Strange Chess Board

---
+++ ASCIS/Weekly_Contest_4/D_Strange_Chess_Board.cpp
@@ -0,0 +1,183 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+vll prime_factors(ll n) {
+    vll primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.push_back(n);
+    return primes;
+}
+
+set<ll> prime_factors_set(ll n) {
+    set<ll> primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes.insert(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        primes.insert(n);
+    return primes;
+}
+
+unordered_map<ll, ll> prime_factors_map(ll n, set<ll> &s) {
+    unordered_map<ll, ll> primes;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            primes[i]++;
+            s.insert(i);
+            n /= i;
+        }
+    }
+    if (n > 1) {
+        s.insert(n);
+        primes[n]++;
+    }
+    return primes;
+}
+
+ll binary_search_n(ll s, ll e, ll k) {
+    ll middle = (s + e) / 2;
+    if (k == middle)
+        return 1;
+    else if (k < middle)
+        return 1 + binary_search_n(s, middle - 1, k);
+    else
+        return 1 + binary_search_n(middle + 1, e, k);
+}
+
+vll prefix_sum(vll v) {
+    v.insert(v.begin(), 0);
+    vll s(v.size());
+    for (int i = 1; i < v.size(); ++i) {
+        s[i] += v[i] + s[i - 1];
+    }
+    return s;
+}
+
+ll sum_range(int s, int e, vll &preSum) {
+    return preSum[e] - preSum[s - 1];
+}
+
+ll binary_search_increasing(ll s, ll e, ll k, const vll &v) {
+    if (e < s)
+        return -1;
+
+    ll middle = s + (e - s) / 2;
+    if (k == v[middle])
+        return middle;
+    else if (k < v[middle])
+        return binary_search_increasing(s, middle - 1, k, v);
+    else
+        return binary_search_increasing(middle + 1, e, k, v);
+}
+
+ll binary_search_decreasing(ll s, ll e, ll k, const vll &v) {
+    if (e < s)
+        return -1;
+
+    ll middle = s + (e - s) / 2;
+    if (k == v[middle])
+        return middle;
+    else if (k > v[middle])
+        return binary_search_decreasing(s, middle - 1, k, v);
+    else
+        return binary_search_decreasing(middle + 1, e, k, v);
+}
+
+bool isPrime(ll n) {
+    if (n == 2)return true;
+    if (n <= 2 || n % 2 == 0)return false;
+    for (ll i = 3; i * i <= n; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool isPowerOfTwo(ll n) {
+    ll x = n - 1;
+    return (n & (x)) == 0;
+}
+
+ll power(ll x, ll y, ll mod = 1) {
+    ll ans = 1;
+    for (ll i = 0; i < y; i++) {
+        ans = ((ans % mod) * (x % mod)) % mod;
+    }
+    return ans;
+}
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    vector<vector<char>> v(n, vector<char>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> v[i][j];
+        }
+    }
+    ll cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        bool valid = true;
+        for (int j = 0; j < m; ++j) {
+            if (v[i][j] == 'B') {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) {
+            for (int k = 0; k < m; k++) {
+                if (v[i][k] == 'W') {
+                    cnt++;
+                    v[i][k] = '.';
+                }
+            }
+        }
+    }
+    for (int j = 0; j < m; ++j) {
+        bool valid = true;
+        for (int i = 0; i < n; ++i) {
+            if (v[i][j] == 'B') {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) {
+            for (int k = 0; k < n; k++) {
+                if (v[k][j] == 'W') {
+                    cnt++;
+                    v[k][j] = '.';
+                }
+            }
+        }
+    }
+    cout << n * m - cnt;
+    return 0;
+}
+/*
+
+3 5
+BBBBB
+WWWWW
+BBBBB
+
+ */
