b5c0b7a2cd90e2a9e3e4149421c8f657ea4c03e2
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2025-02-14 14:33:14 +0200

    A - Skibidus and Amog'u

---
+++ Contests/Codeforces Round 1003 (Div. 4)/A. Skibidus and Amog'u.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        string s;
+        cin>>s;
+        for(int i=0;i<s.length()-2;i++) {
+            cout<<s[i];
+        }
+        cout<<"i"<<endl;
+    }
+    return 0;
+}
