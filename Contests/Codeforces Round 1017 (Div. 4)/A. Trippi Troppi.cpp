de0cecd1014d5ae3553e5779340a54aaebc7a142
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2025-04-13 21:33:21 +0200

    A - Trippi Troppi

---
+++ Contests/Codeforces Round 1017 (Div. 4)/A. Trippi Troppi.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        string s;
+        cin>>s;
+        cout<<s[0];
+        cin>>s;
+        cout<<s[0];
+        cin>>s;
+        cout<<s[0];
+        cout<<endl;
+    }
+
+    return 0;
+}
