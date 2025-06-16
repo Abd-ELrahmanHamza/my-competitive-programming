6f0ceb408b15a761392d1f7733f0008e66d0e1cf
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2025-06-16 21:46:02 +0300

    [2016] Maximum Difference Between Increasing Elements

---
+++ leetcode/2016.maximum-difference-between-increasing-elements.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2016 lang=cpp
+ *
+ * [2016] Maximum Difference Between Increasing Elements
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
