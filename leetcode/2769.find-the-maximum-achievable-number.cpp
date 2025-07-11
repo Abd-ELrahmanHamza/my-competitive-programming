f6ffcbcdd631610e6b1d331ee9d880b39f07e4c2
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2025-07-11 19:01:38 +0300

    [2769] Find the Maximum Achievable Number

---
+++ leetcode/2769.find-the-maximum-achievable-number.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=2769 lang=cpp
+ *
+ * [2769] Find the Maximum Achievable Number
+ */
+
+// @lc code=start
+class Solution
+{
+  public:
+    int theMaximumAchievableX(int num, int t)
+    {
+        return num + 2 * t;
+    }
+};
+// @lc code=end
