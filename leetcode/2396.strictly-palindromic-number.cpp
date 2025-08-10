8c4c05acf86e4414e1e8ae0a5917613337e5a6a6
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2025-08-10 21:02:27 +0300

    [2396] Strictly Palindromic Number

---
+++ leetcode/2396.strictly-palindromic-number.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2396 lang=cpp
+ *
+ * [2396] Strictly Palindromic Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
+// @lc code=end
+
